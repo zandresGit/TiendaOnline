3a32a392ae8aa31a2cd276e8340a1f797fc8712f
Richi <zandres7r@gmail.com>
2022-11-09 09:17:02 -0500

    Se agrega la clase barrio

---
+++ TiendaOnlineWeb/Models/Barrios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TiendaOnlineWeb.Models
+{
+    public class Barrios
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
+        public string barrio { get; set; }
+    }
+}
